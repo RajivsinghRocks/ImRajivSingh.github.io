5432367e32ca0f28bf26f935306633eba41461fe
Rajiv singh <49805637+RajivsinghRocks@users.noreply.github.com>
2022-09-29 18:15:44 +0530

    Delete script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-
-/*=================================Typing Animation =====================*/
-var typed =new Typed(".typing",{
-    strings:[ " ","Web Designer","UI/UX Designer","Software Tester"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
